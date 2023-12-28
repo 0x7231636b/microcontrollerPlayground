e4c6c428c3064342f9d7f401c007efa10e9806ff
Markus Hallmann <markus@hallmann.pw>
2023-12-28 16:46:56 +0100

    init platformio project

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+#define LOG(x) Serial.println(x);
+#define LOG_VALUE(x, y) Serial.print(x); LOG(y)
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+  LOG_VALUE("result is: ", result);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(1000);
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
